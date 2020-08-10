5aeb20cb13256cc2a6610007db285289bf7b4546
panjie <3792535@qq.com>
2020-08-10 23:25:26 +0800

    upate

--- src/test/java/club/codedemo/hibernatecriteriaqueries/StudentServiceTest.java
+++ src/test/java/club/codedemo/hibernatecriteriaqueries/StudentServiceTest.java
@@ -1,7 +1,6 @@
 package club.codedemo.hibernatecriteriaqueries;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
